f346e23b763f403a9f548910d5d734cd6f41dacc
marcos-vin1 <marcos.tavares.silva@escola.pr.gov.br>
2026-02-23 12:36:32 +0000

    CRIAR VARIAVEIS PARA PESSOAS E PRODUTOS

---
+++ variavel/variavel-pessoa.js
@@ -0,0 +1,29 @@
+// , var, let, const (valores reservados da linguagem)
+// 0-9, 0,7, 0-9,A,B,C,D,E,F
+
+let nome = 'nachor'
+let sobrenome = 'bache'
+const cpf = "052986892.05"
+const rg = '101212'
+let idade = 34
+let solteiro = true
+let nota = 8 + 5
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(solteiro)
+console.log(nota)
+
+
+
+
+console.log(typeof nome)
+console.log(typeof sobrenome)
+console.log(typeof cpf)
+console.log(typeof rg)
+console.log(typeof idade)
+console.log(typeof solteiro)
+console.log(typeof nota)
---
+++ variavel/variavel-produto.js
@@ -0,0 +1,18 @@
+/**
+ */
+let nomeproduto = 'spato'
+
+let nome2produto ='calsa'
+
+let nome3produto ='camisa'
+
+let nome4produto ='short'
+
+let nomeproduto5 ='sandalia'
+
+
+console.log("nome: " + nomeproduto)
+console.log("nome: " + nome2produto)
+console.log("nome: " + nome3produto)
+console.log("nome: " + nome4produto)
+console.log("nome: " + nomeproduto5)
