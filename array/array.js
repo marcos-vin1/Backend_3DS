4e730e63b35faf420a9e38e2c3a6bc26d1f0ba05
marcos-vin1 <marcos.tavares.silva@escola.pr.gov.br>
2026-03-04 11:48:43 +0000

    introdução arrys em js

---
+++ array/array.js
@@ -0,0 +1,19 @@
+// grupo de valores indexados a partir 
+// do ZERO até infinito 
+// ieteravel, homogêneo/hetorogêneo
+//possui métodos nativos
+
+const time = ["julius", "cris"]
+// indices      0          1
+
+time[5] // undefined
+time[6] = "victor"  // atribuição dinâmicos
+
+for (const jogador of time) {
+    console.log(jogador)
+}
+    
+    
+
+console.log(time)
+console.log(time[1])
