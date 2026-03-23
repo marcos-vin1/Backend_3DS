614cc79e59c5b2aaa15461b0479150ab3f450f15
marcos-vin1 <marcos.tavares.silva@escola.pr.gov.br>
2026-03-23 12:03:22 +0000

    aprendi sobre objestos e como colocalos em uma linha só

---
+++ obj-01.js/obj-02.js
@@ -0,0 +1,37 @@
+//crie um objeto do tema que escolher
+// crie pelo menos 5 propriedades
+//faça uma impressão para humanos!
+//ex. console.log("nome do jogador:", + "jogador.nome")
+
+const cmapeao = [
+{
+personagem1: "atrox", lane1: "top",}
+{personagem2: "kindred", lane2: "selva",}
+
+{personagem3: "aurelion", lane3: "meio",}
+
+{personagem4: "jinx", lane4: "inferior",}
+
+{personagem5: "blitz",lane5: "suport"}
+
+]
+cmapeao.personagem1 = "atrox"
+cmapeao.lane1 = "top"
+cmapeao.personagem2 = "kindred"
+cmapeao.lane2 = "selva"
+cmapeao.personagem3 = "aurelion"
+cmapeao.lane3 = "meio"
+cmapeao.personagem4 = "jinx"
+cmapeao.lane4 = "inferior"
+cmapeao.personagem5 = "blitz"
+cmapeao.lane5 = "suport"
+console.log(cmapeao.personagem1);
+console.log(cmapeao.lane1);
+console.log(cmapeao.personagem2);
+console.log(cmapeao.lane2);
+console.log(cmapeao.personagem3);
+console.log(cmapeao.lane3);
+console.log(cmapeao.personagem4);
+console.log(cmapeao.lane4);
+console.log(cmapeao.personagem5);
+console.log(cmapeao.lane5)
