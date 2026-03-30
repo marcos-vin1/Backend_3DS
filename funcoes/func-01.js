ae1de3a50d22b0865a5b1713bab678f095481645
marcos-vin1 <marcos.tavares.silva@escola.pr.gov.br>
2026-03-30 11:00:24 +0000

    aprendi sobre função

---
+++ funcoes/func-01.js
@@ -0,0 +1,17 @@
+// funçoes são blocos de codigo reaproveitáveis 
+// podem ou Não receber parâmetros 
+// podem ou não retornar valores
+
+// declaração
+
+function dizOla(){
+    console.log("olá")
+}
+    function olapessoa(nome){
+console.log("ola, " + nome)
+}
+
+// invocação
+dizOla()
+olapessoa("luana")
+olapessoa("mano")
---
+++ funcoes/func-02.js
@@ -0,0 +1,21 @@
+// crie 4 funções para operções (+, -, *, /)
+// calcule e imprime resultado
+
+function subtracao(n1, n2){
+const resultado = n1 + n2
+
+console.log("resultado da subtraação: " + resultado) 
+}
+
+//invocação
+subtracao(5,2)
+subtracao(-50, 10)
+
+
+function multiplicacao(n1 , n2){
+const resultado = n1 * n2
+console.log("resultado da multiplicação " + resultado)
+}
+
+multiplicacao(5, 2)
+multiplicacao(20, 23)
