adfac6953312b4db216625b1d8567d55e01cddff
marcos-vin1 <marcos.tavares.silva@escola.pr.gov.br>
2026-04-07 12:17:53 +0000

    áperndi sobre função

---
+++ funcoes/func-04.js
@@ -0,0 +1,12 @@
+// sequencias com resultado 
+// claussula/ termo 'return'
+// isso permite fazer operações
+//sequencias com o resultado
+// armazenar funções em variaveis 
+
+function sobraDosalario(meuSalario){
+    const contaDeluz = 150.0
+    const aluguel = 1400.0
+    const resto = meuSalario - (contaDeluz+aluguel)
+    console.log()
+} 
