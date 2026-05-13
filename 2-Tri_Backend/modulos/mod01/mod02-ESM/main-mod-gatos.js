d0480ed55771b9289a6981ba272a8aa4df7b9464
marcos-vin1 <marcos.tavares.silva@escola.pr.gov.br>
2026-05-13 11:41:29 +0000

    aprendemos usar o main, e usamos o main gatos.

---
+++ 2-Tri_Backend/modulos/mod01/mod02-ESM/main-mod-gatos.js
@@ -0,0 +1,7 @@
+import {banhoGato, tosaGato, alimetoGato} from './mod-gatos.js'
+
+let racaViralata = 'Vira-lata'
+
+console.log(banhoGato('Siamês', 'Completo'))
+console.log(tosaGato('polones', 'baixo'))
+console.log(alimetoGato('xinguilingue', 'avenger'))
--- 2-Tri_Backend/modulos/mod01/mod02-ESM/mod-gatos.js
+++ 2-Tri_Backend/modulos/mod01/mod02-ESM/mod-gatos.js
@@ -1,1 +1,19 @@
-let raçaVirada = 'vira'
+export function banhoGato(racaGato, TipoBanho) {
+return `Gato ${racaGato} fez um banho tipo: ${TipoBanho}`   
+}
+
+
+export function tosaGato(racaGato, tipoTosa){
+return `Gato ${racaGato} fez um banho tipo: ${tipoTosa}`
+}
+export function alimetoGato(racaGato, tiRacao) {
+return `Gato ${racaGato} fez um banho tipo: ${tiRacao}`    
+}
+
+
+
+
+
+
+
+
